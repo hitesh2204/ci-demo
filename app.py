0fdca351d9b270e1426b62a8f0db1e2f9451b32e
hitesh <hitesh.yerekar@gmail.com>
2024-08-20 16:53:30 +0530

    multiplication code added.

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ def sub(a, b):
     return a - b
 
 
+def mul(a, b):
+    return a * b
+
+
 result1 = add(4, 5)
 result2 = sub(10, 6)
+result3 = mul(5, 6)
 
 print(result1)
 print(result2)
+print(result3)
