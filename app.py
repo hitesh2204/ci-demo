f4e4ca5e11168736d97c8b0398993ba80818c877
hitesh <hitesh.yerekar@gmail.com>
2024-08-20 16:51:12 +0530

    corrected code.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-def add(a ,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
 def sub(a, b):
-    return a-b
+    return a - b
 
 
-result1=add(4, 5)
-result2=sub(10, 6)
+result1 = add(4, 5)
+result2 = sub(10, 6)
 
 print(result1)
 print(result2)
