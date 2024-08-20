8e9a96c2c1f7fb61eb72152607f6f18c406f9734
hitesh <hitesh.yerekar@gmail.com>
2024-08-20 17:04:50 +0530

    updated code.

--- app.py
+++ app.py
@@ -14,10 +14,15 @@ def div(a, b):
     return a / b
 
 
+def sample(a):
+    return 100 - a
+
+
 result1 = add(4, 5)
 result2 = sub(10, 6)
 result3 = mul(5, 6)
 result4 = div(10, 5)
+result5 = sample(10)
 
 print(result1)
 print(result2)
